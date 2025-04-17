3fec86cdc66dc6c54514bdece5b108ef35bece12
Henri2k5 <henrikyuban@gmail.com>
2025-04-17 00:24:56 -0300

    Atualização

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_techtaste/data/restaurant_data.dart';
+import 'package:flutter_techtaste/model/restaurant.dart';
 import 'package:flutter_techtaste/ui/_core/app_theme.dart';
 import 'package:flutter_techtaste/ui/splash/splash_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-   runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  RestaurantData restaurantData = RestaurantData();
+  await restaurantData.getRestaurants();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) {
+            return restaurantData;
+          },
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,9 +28,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.appTheme,
-      home: SplashScreen(),
-    );
+    return MaterialApp(theme: AppTheme.appTheme, home: SplashScreen());
   }
 }
